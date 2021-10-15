9dac79d4494834d1a442c8422fa74297e7fc9e7f
javi-de <javier_de@hotmail.com>
2021-10-15 11:57:18 +0200

    ej 10

--- Tanda5/ejercicio7_app.js
+++ Tanda5/ejercicio7_app.js
@@ -1,24 +1,34 @@
-var arrLetras = new Array('T', 'R', 'W', 'A', 'G', 
-                           'M', 'Y', 'F', 'P', 'D', 
-                           'X', 'B', 'N', 'J', 'Z', 
-                           'S', 'Q', 'V', 'H', 'L', 
-                           'C', 'K', 'E', 'T'
+var arrLetras = new Array('T', 
+                          'R', 'W', 'A', 'G', 'M', 
+                          'Y', 'F', 'P', 'D', 'X', 
+                          'B', 'N', 'J', 'Z', 'S', 
+                          'Q', 'V', 'H', 'L', 'C', 
+                          'K', 'E', 'T'
                         ); 
 
 function validarDNI(dni){
-    let numero= substr(dni, 0, 8);
-    let letra= substr(dni, 9);
-    console.log(numero);
-    console.log(letra);
-    let letraValidada= parseInt(11%2);
-    console.log(letraValidada);
-    codigo;/*  */
 
-// codigo;
+    var patronDNI= /^[0-9]{8}[A-Z]{1}$/;
     
+    if(patronDNI.test(dni)){
+        let numero= dni.substr(0, 8);
+        let letra= dni.substr(8);
 
+        //console.log(numero);
+        //console.log(letra);
+
+        let letraValidada= parseInt(numero)%23;
+        //console.log(letraValidada);
+    
+        if(letra== arrLetras[letraValidada]){
+            alert("DNI correcto");
+        }else{
+            alert("Letra incorrecta");
+        }
+    }else{
+        alert("DNI incorrecto");
+    }
 }
 
-validarDNI("77577736R");
-//console.log(letra);
-
+//validarDNI("77577736R");
+//validarDNI("72829462J");

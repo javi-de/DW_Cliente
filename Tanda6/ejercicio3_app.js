83556b9dfea8f57d1613f8b17bdd9c265cfa24fa
javi-de <javier_de@hotmail.com>
2021-10-18 11:32:24 +0200

    empezando 6.4

---
+++ Tanda6/ejercicio3_app.js
@@ -0,0 +1,18 @@
+function mostrarSelect(){
+    let resultado= "Has seleccionado ";
+
+    let selectProc= document.getElementById("procesadores");
+    let txtProc= selectProc.options[selectProc.selectedIndex].value;
+    resultado+= "el procesador " + txtProc + " con ";
+
+            
+    let selectSOs= document.getElementById("SOs");
+
+    for (let i= 0; i< selectSOs.options.length; i++) {
+        const opcion = selectSOs.options[i];
+        if(opcion.selected)    
+            resultado+= "el SOs " + opcion.value + ", ";
+    }
+       
+    alert(resultado);
+}

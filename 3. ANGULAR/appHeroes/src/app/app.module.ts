b72e232ef666d573ff701eacda1f8247e9009bfb
javi-de <javier_de@hotmail.com>
2022-01-18 11:46:47 +0100

    ejercicio 1 HECHO

---
+++ 3. ANGULAR/appHeroes/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
+
+//Routing
+import { APP_ROUTING } from './app.routes';
+
+//Services
+
+//Components
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { AboutComponent } from './components/about/about.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    HeroesComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    APP_ROUTING
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ 3. ANGULAR/appHeroes/src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { RouterModule, Routes } from '@angular/router';
+//import { Component } from '@angular/core';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+
+const APP_ROUTES: Routes= [
+    //{ path: 'routePath', component: Component },
+    { path: '**', pathMatch: 'full', redirectTo:'' },
+    { path: 'home', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+];
+
+export const APP_ROUTING= RouterModule.forRoot(APP_ROUTES, { useHash: true });

02926bea8b8757eaddaca8f5c628dd8024914fcb
javi-de <javier_de@hotmail.com>
2022-01-19 11:19:47 +0100

    Actualizacion Ejercicio 2

--- 3. ANGULAR/appHeroes/src/app/app.routes.ts
+++ 3. ANGULAR/appHeroes/src/app/app.routes.ts
@@ -4,10 +4,10 @@ import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 
 const APP_ROUTES: Routes= [
-    //{ path: 'routePath', component: Component },
-    { path: '**', pathMatch: 'full', redirectTo:'' },
-    { path: 'home', component: HomeComponent },
-    { path: 'about', component: AboutComponent },
+    //{ path: 'routePath', component: Component},
+    { path: '**', pathMatch: 'full', redirectTo:'home'},
+    { path: 'home', component: HomeComponent},
+    { path: 'about', component: AboutComponent},
 ];
 
 export const APP_ROUTING= RouterModule.forRoot(APP_ROUTES, { useHash: true });
